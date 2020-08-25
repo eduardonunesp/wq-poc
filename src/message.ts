570fb3c2c79db064b19b2d7ba7468dd2db417363
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2020-08-25 20:16:29 -0300

    Docs

--- src/message.ts
+++ src/message.ts
@@ -1,20 +1,45 @@
 import { v4 } from 'uuid'
 
+/**
+ * Possible states of the message
+ *
+ * READY for new messages ready to me consumed
+ * PROCESSING for messages already "locker" by a consumer
+ * DONE for message already consumed
+ */
 export enum MessageState {
   READY,
   PROCESSING,
   DONE,
 }
 
+/**
+ * The message class describes the unique message id, body with the contents of the message,
+ * also the message state, and a timeout timer for callback when the message is not confirmed
+ */
 export class Message {
   private _id: string
   private _state: MessageState = MessageState.READY
   private _timer?: NodeJS.Timeout
 
+  /**
+   * Creates a new Message to be inserted into the queue
+   *
+   * @param body any Body of the message
+   * @param timeoutCb Function If the function is on processing for long period the callback is called
+   * @param timeout number Amount in milliseconds to consider a message expired on processing
+   */
   constructor(public body: any, public timeoutCb: Function, private timeout: number = 1000) {
     this._id = v4()
   }
 
+  /**
+   * New state to be set on this message
+   *
+   * Important to notice that PROCESSING state triggers the timeout counter
+   * if the message not confirmed before timeout the function timeoutCb will be called
+   * Any state different of PROCESSING with clear the timeout timer
+   */
   set state(newState: MessageState) {
     if (newState === MessageState.PROCESSING) {
       this._timer = setTimeout(() => {
@@ -34,10 +59,16 @@ export class Message {
     this._state = newState
   }
 
+  /**
+   * Returns the message UUID
+   */
   get id(): string {
     return this._id
   }
 
+  /**
+   * Returns the message current state
+   */
   get state(): MessageState {
     return this._state
   }
--- src/queue.ts
+++ src/queue.ts
@@ -1,23 +1,56 @@
 import { Message, MessageState } from './message'
 
+/**
+ * Queue is the class responsible for manage the messages and processing them
+ * in a FIFO style. New messages are created and pushed into the queue to be
+ * consumed and confirmed
+ */
 export class Queue {
+  // Holds the queue FIFO using Push/Shift methods
   messagesReady: Array<Message>
+
+  // Messages on processing state, can be removed or inserteed back on queue if not confirmed
   messagesProcessing: Map<string, Message>
 
+  /**
+   * Creates a new queue
+   *
+   * @param name string The name of the queue as unique indentification
+   */
   constructor(public name: string) {
     this.messagesReady = new Array<Message>()
     this.messagesProcessing = new Map<string, Message>()
   }
 
+  /**
+   * Returns the lenght of the queue for messages that are ready to be consumed
+   *
+   * @returns number Length of the queue
+   */
   LengthOfAvailable(): number {
     return this.messagesReady.length
   }
 
-  MessageTimeout(message: Message) {
+  /**
+   * Internal callback used by the messages, when called removes the message from
+   * the messages on processing and push back to the queue
+   *
+   * @param message Message to timeout
+   */
+  protected MessageTimeout(message: Message) {
     this.messagesProcessing.delete(message.id)
     this.messagesReady.push(message)
   }
 
+  /**
+   * Confirms that a message is consumed with success, the message will be removed
+   * from the messages on processing and marked as DONE
+   *
+   * @throws Error if the message id isn't available to confirm, which can happen if
+   * the message is done or awaiting for confirmation
+   *
+   * @param messageID string Message id
+   */
   Confirm(messageID: string) {
     if (!this.messagesProcessing.has(messageID)) {
       throw Error(`Message with ID ${messageID} it's not available for confirm`)
@@ -27,6 +60,12 @@ export class Queue {
     this.messagesProcessing.delete(messageID)
   }
 
+  /**
+   * Pushes a new message to the end of the queue, later can be consumed by a consumer
+   *
+   * @param body any Body of the message to be pushed into the queue
+   * @param timeout number The value in ms to the amount of time to the message expire if not confirmed
+   */
   Push(body: any, timeout: number = 1000): string {
     const message = new Message(body, this.MessageTimeout.bind(this), timeout)
 
@@ -36,6 +75,14 @@ export class Queue {
     return message.id
   }
 
+  /**
+   * Pops the message from the queue
+   *
+   * If the amount of messages is greater than the queue length it will return all messages available
+   *
+   * @param amount number Amount of messages to be consumed from the queue
+   * @returns Array<Message> Returns array of messages account with the length requested on amount param
+   */
   PopFront(amount: number = 10): Array<Message> {
     const messages = Array<Message>()
 
@@ -55,6 +102,9 @@ export class Queue {
   }
 }
 
+/**
+ * QueueManager is just a container to manage different queues by an ID/name
+ */
 export class QueueManager {
   queues: Map<string, Queue> = new Map<string, Queue>()
 
@@ -64,6 +114,7 @@ export class QueueManager {
    * @throws Error if the queue already exists with the same name
    *
    * @param queueName string Name of the new queue
+   * @returns Queue Returns a new queue ready to be used
    */
   newQueue(queueName: string): Queue {
     if (this.queues.has(queueName)) {
